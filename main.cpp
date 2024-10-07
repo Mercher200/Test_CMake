52d02eec6ff29587d37d1a1dc122d796ea1e5c06
Dmitriy P. <60214129a@gmail.com>
2024-10-08 01:31:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
-#include "second_lib/mul.hpp"
-#include "first_lib/sum.hpp"
+#include "mul.hpp"
+#include "sum.hpp"
 
 int main(){
     int a = sum(1 , 2);
     int b = mul(2 , 3);
-    printf("a =%d", a);
-     printf("b =%d", b);
+
+    std::cout<< "a =" << a << "\n";
+    std::cout<< "b =" << b << "\n";
+
     return 0;
 }
